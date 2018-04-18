1273d41bebef9a9bde4ec46b645adc4a388a9433
Ice Zero <ziv3@outlook.com>
2018-04-18 15:40:31 +0800

    FIX: personal nav

--- src/components/common/PersonalNav/PersonalNav.vue
+++ src/components/common/PersonalNav/PersonalNav.vue
@@ -1,35 +1,27 @@
 <template>
-  <nav>
-    <Menu mode="horizontal" :theme="theme1" active-name="1">
-      <MenuItem name="1">
-        <Icon type="ios-paper"></Icon>
-        内容管理
-      </MenuItem>
-      <MenuItem name="2">
-        <Icon type="ios-people"></Icon>
-        用户管理
-      </MenuItem>
-      <Submenu name="3">
-        <template slot="title">
-          <Icon type="stats-bars"></Icon>
-          统计分析
-        </template>
-        <MenuGroup title="使用">
-          <MenuItem name="3-1">新增和启动</MenuItem>
-          <MenuItem name="3-2">活跃分析</MenuItem>
-          <MenuItem name="3-3">时段分析</MenuItem>
-        </MenuGroup>
-        <MenuGroup title="留存">
-          <MenuItem name="3-4">用户留存</MenuItem>
-          <MenuItem name="3-5">流失用户</MenuItem>
-        </MenuGroup>
-      </Submenu>
-      <MenuItem name="4">
-        <Icon type="settings"></Icon>
-        综合设置
-      </MenuItem>
-    </Menu>
-  </nav>
+  <Menu mode="horizontal" :theme="navTheme" active-name="1">
+  
+    <MenuItem name="1">
+      <Icon type="ios-home"></Icon>
+      首页
+    </MenuItem>
+  
+    <MenuItem name="2">
+      <Icon type="ios-search"></Icon>
+      搜索
+    </MenuItem>
+  
+    <MenuItem name="3">
+      <Icon type="merge"></Icon>
+      项目管理
+    </MenuItem>
+  
+    <MenuItem name="4">
+      <Icon type="ios-paperplane"></Icon>
+      专利发布
+    </MenuItem>
+  
+  </Menu>
 </template>
 
 <script>
@@ -45,7 +37,7 @@ export default {
   },
   data() {
     return {
-      theme1: 'light',
+      navTheme: 'primary',
     };
   },
 };
